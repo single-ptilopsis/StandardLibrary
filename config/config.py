a032b20ecc207e0126569a0b0905922b2a787eda
single-ptilopsis <single-ptilopsis@foxmail.com>
2022-09-16 14:37:58 +0800

    update: 优化代码结构

--- config/config.py
+++ config/config.py
@@ -3,15 +3,16 @@
 StandardLibrary v1.2
 配置读取，并转化为对象
 TODO support network file？
+TODO support Unino Option
 """
 
-__all__ = ["config", "sync"]
+__all__ = ['config', 'sync']
 
 import os
 import re
 import json
 # 从typing导入类提示的基类，以判断类提示
-from typing import _GenericAlias, Optional
+from typing import _GenericAlias, Optional, Any
 
 try:
     import yaml
@@ -22,11 +23,11 @@ except ImportError as yaml_import_error:
 class ConfigError(Exception):
     """
     配置报错
-    TODO 优化报错显示
+    TODO 优化报错提示
     """
 
-    def __init__(self, expect: object, k: str = None, reason: str = "", sub_expect: str = ""):
-        self.expect = str(expect) + "/" + sub_expect if sub_expect else str(expect)
+    def __init__(self, expect: object, k: str = None, reason: str = '', sub_expect: str = ''):
+        self.expect = str(expect) + '/' + sub_expect if sub_expect else str(expect)
         self.k = k
         self.reason = reason
 
@@ -35,9 +36,9 @@ class Propagate:
     """
     基类，用于支持上报(文件同步)功能
     """
-    _father: Optional["Propagate"]
+    _father: Optional['Propagate']
 
-    def __init__(self, father: Optional["Propagate"] = None):
+    def __init__(self, father: Optional['Propagate'] = None):
         self._father = father
 
     def _propagate(self):
@@ -46,7 +47,10 @@ class Propagate:
 
 
 class PropagateCallback(Propagate):
-    # 配置修改事件的回调函数
+    """
+    配置修改事件的回调函数，用于支持上报(文件同步)
+    """
+
     def __init__(self, config, callback):
         """
         :param config: 配置对象
@@ -60,6 +64,11 @@ class PropagateCallback(Propagate):
 
 
 class _List(Propagate, list):
+    """
+    list类，添加propagate方法以支持上报
+    可用dump方法序列化为一般list
+    """
+
     def __init__(self, seq=(), father=None):
         Propagate.__init__(self, father)
         list.__init__(self, seq)
@@ -105,18 +114,25 @@ class _List(Propagate, list):
 
 
 class _Dict(Propagate, dict):
+    """
+    dict类，添加propagate方法以支持上报
+    可用getattr方法调用dict.getitem
+    e.g. d = _Dict({'a': 1}); d.a -> 1
+    可用dump方法序列化为一般dict
+    """
+
     def __init__(self, seq=(), father=None):
         Propagate.__init__(self, father)
         dict.__init__(self, seq)
 
     def __getattr__(self, key):
-        if key.startswith("_"):
+        if key.startswith('_'):
             return super().__getattribute__(key)
         else:
             return super().__getitem__(key)
 
     def __setattr__(self, key, value):
-        if key.startswith("_"):
+        if key.startswith('_'):
             super().__setattr__(key, value)
         else:
             super().__setitem__(key, value)
@@ -143,96 +159,97 @@ class _Dict(Propagate, dict):
         return {k: (v.dump() if isinstance(v, _List) or isinstance(v, _Dict) else v) for k, v in self.copy().items()}
 
 
-def dict2obj(dic, father=None):
+def isbuildin(_type) -> bool:
     """
-    将字典转为类
-    使用此方法转换的对象不能使用hasattr
-    :param dic: 字典
-    :param father: 父配置，用于同步文件
-    :return: 对象
+    判断一个类型是否是内置类
+    :param _type: 类 str/int ...
+    :return bool
     """
-    if not isinstance(dic, dict):
-        return dic
-    d = _Dict(father=father)
-    for k, v in dic.items():
-        if isinstance(v, list):
-            v = _List((dict2obj(i, father=father) for i in v), father=father)
-        d[k] = dict2obj(v, father=d)
-    return d
+    return _type in [str, int, list, dict]
 
 
-def isbuildin(_type):
-    # 判断一个类型是否是内置类
-    return _type in [str, int, list, dict] or isinstance(_type, _GenericAlias)
+def istyping(_type) -> bool:
+    """
+    判断一个类型是否是类标注
+    :param _type: 类 List[str]...
+    :return: bool
+    """
+    return isinstance(_type, _GenericAlias)
 
 
-def get_default(obj: object):
-    return {k: v for k, v in obj.__dict__.items() if not k.startswith("__")}
+def get_default(obj: object) -> dict:
+    """
+    获取对象的默认值, 对于函数类默认值，会在取用时调用(要求无参)
+    :param obj: 对象
+    :return:
+    """
+    return {k: v for k, v in obj.__dict__.items() if not k.startswith('__')}
 
 
-def get_value(value, _type, father=None):
+def buildin2expect(value, _type, father=None):
+    """
+    将值转为内置类型
+    :param value: 值
+    :param _type: 期望类型
+    :param father: 父配置，用于支持上报
+    :return: 配置
+    """
     if _type == str:
         return str(value)
     elif _type == int:
         if isinstance(value, int):
             return value
-        elif isinstance(value, str) and re.search("^[0-9 *]+$", value):
+        elif isinstance(value, str) and re.search('^[0-9 *]+$', value):
+            # e.g. 3600 * 8
             return eval(value)
         else:
             int(value)
-    elif _type == list:
-        return _List((dict2obj(i, father=father) for i in value), father=father)
-    elif _type == dict:
-        return dict2obj(value, father=father)
-    elif isinstance(_type, _GenericAlias):
-        if not isinstance(value, _type.__origin__):
-            raise TypeError(f"need {_type.__origin__} but {type(value)} is given")
-        if _type.__origin__ == list:
-            if isbuildin(_type.__args__[0]):
-                return _List((get_value(i, _type.__args__[0], father=father) for i in value), father=father)
-            else:
-                return _List((dict2expect(i, _type.__args__[0], father=father) for i in value), father=father)
-        elif _type.__origin__ == dict:
-            if isbuildin(_type.__args__[1]):
-                return _Dict(
-                    ((get_value(k, _type.__args__[0]), get_value(v, _type.__args__[1], father=father)) for k, v in
-                     value.items()), father=father)
-            else:
-                return _Dict(
-                    ((get_value(k, _type.__args__[0]), dict2expect(v, _type.__args__[1], father=father)) for k, v in
-                     value.items()), father=father)
-        else:
-            # TODO else type ?
-            return dict2obj(value)
+    elif _type == list or _type == dict:
+        return config2obj(value, father=father)
     else:
-        # shouldn't happen
-        raise TypeError(f"Unknown type {_type}")
+        raise TypeError
 
 
-def dict2expect(dic: dict, expect: object, father=None):
+def typing2expect(value, _type: _GenericAlias, father=None):
+    if not isinstance(value, _type.__origin__):
+        # 当类型与标注类不同时报错
+        raise TypeError(f'need {_type.__origin__} but {type(value)} is given')
+    if _type.__origin__ == list:
+        return _List((config2expect(i, _type.__args__[0], father=father) for i in value), father=father)
+    elif _type.__origin__ == dict:
+        return _Dict(
+            ((buildin2expect(k, _type.__args__[0]), config2expect(v, _type.__args__[1], father=father)) for k, v in
+             value.items()), father=father)
+    else:
+        # TODO else type ?
+        return config2obj(value)
+
+
+def dict2obj(obj: dict, expect: type, father=None):
     """
-    将字典转为类，并根据期望对象提供默认值/类型检查
+    将字典配置转为预期对象
     使用此方法转换的对象不能使用hasattr
-    :param dic: 字典
-    :param expect: 期望对象
-    :param father: 父配置，用于同步文件
-    :return: 对象
+    :param obj: 配置
+    :param expect: 预期类
+    :param father: 福配置，用于同步文件
+    :return: 配置对象
     """
     d = _Dict(father=father)
     default = get_default(expect)
     k = None
     try:
-        if "__annotations__" in expect.__dict__:
+        if '__annotations__' in expect.__dict__:
+            # 类标注处理
             for k, _type in expect.__annotations__.items():
-                if k in dic:
-                    if isbuildin(_type):
-                        d[k] = get_value(dic[k], _type, father=d)
-                    else:
-                        d[k] = dict2expect(dic[k], _type, father=d)
+                if k in obj:
+                    d[k] = config2expect(obj[k], _type)
                 elif k in default:
-                    d[k] = default[k]
+                    d[k] = get_value(default[k], father=father)
+                elif not isbuildin(_type) and not istyping(_type):
+                    d[k] = config2expect({}, _type)
                 else:
-                    raise ConfigError(expect, k, "missing config")
+                    raise ConfigError(expect, k, 'missing config')
+
     except ValueError as err:
         raise ConfigError(expect, k, err.args[0])
     except TypeError as err:
@@ -241,43 +258,101 @@ def dict2expect(dic: dict, expect: object, father=None):
         raise ConfigError(expect, err.k, err.reason, err.expect)
 
     for k, v in default.items():
+        # 无标注默认值
         if k not in d:
-            d[k] = v
+            d[k] = get_value(v, father=d)
+
+    for k, v in obj.items():
+        if k not in d:
+            d = config2obj(v, father=d)
 
     return d
 
 
-def read_config(path: str = "config", raw_path: str = None, expect: object = None, sync: bool = False):
+def get_value(value, father=None):
+    """
+    识别值类型，调用函数或抽取对象默认值
+    :param value: 值
+    :param father: 父配置，用于同步文件
+    :return:
+    """
+    if isinstance(value, type):
+        return config2obj(get_default(value), father=father)
+    elif callable(value):
+        return config2obj(value(), father=father)
+    else:
+        return config2obj(value, father=father)
+
+
+def config2obj(config, father=None):
+    """
+    将配置转为类
+    使用此方法转换的对象不能使用hasattr
+    :param config: 可为str/int/list/dict
+    :param father: 父配置，用于同步文件
+    :return: 对象
+    """
+    if isinstance(config, list):
+        l = _List(father=father)
+        l.extend(config2obj(c, father=l) for c in config)
+        return l
+    elif isinstance(config, dict):
+        d = _Dict(father=father)
+        for k, v in config.items():
+            d[k] = config2obj(v, father=d)
+        return d
+    else:
+        return config
+
+
+def config2expect(config: Any, expect: type, father=None):
+    """
+    将配置转为预期类，并根据期望对象提供默认值/类型检查
+    使用此方法转换的对象不能使用hasattr
+    :param config: 配置
+    :param expect: 期望对象
+    :param father: 父配置，用于同步文件
+    :return: 配置对象
+    """
+    if isbuildin(expect):
+        return buildin2expect(config, expect, father=father)
+    if istyping(expect):
+        return typing2expect(config, expect, father=father)
+    else:
+        return dict2obj(config, expect, father=father)
+
+
+def read_config(path: str = 'config', raw_path: str = None, expect: type = None, sync: bool = False):
     """
     读取配置文件，默认在config文件夹下寻找，可用raw_path通过绝对路径读取
     可省略后缀名，会尝试自动读取，目前支持 .yaml/.json
-    :param path:
     """
-    path = raw_path or os.path.join("config", path)
+    path = raw_path or os.path.join('config', path)
     if not os.path.exists(path):
-        for i in [".yaml", ".json"]:
+        for i in ['.yaml', '.json']:
             if os.path.exists(path + i):
                 path = path + i
                 break
         else:
             raise FileNotFoundError(path)
 
-    with open(path, mode="rt", encoding="utf-8") as f:
-        if path.endswith(".yaml"):
+    with open(path, mode='rt', encoding='utf-8') as f:
+        if path.endswith('.yaml'):
             if not yaml:
                 raise yaml_import_error
             config = yaml.safe_load(f)
-        elif path.endswith(".json"):
+        elif path.endswith('.json'):
             config = json.load(f)
         else:
-            raise TypeError(f"not support config file type {path}")
+            raise TypeError(f'not support config file type {path}')
+
     if expect:
         try:
-            config = dict2expect(config, expect)
+            config = config2expect(config, expect)
         except ConfigError as err:
-            raise ConfigError("%s config error %s: %s" % (err.expect, err.k, err.reason))
+            raise ConfigError('%s config error %s: %s' % (err.expect, err.k, err.reason))
     else:
-        config = dict2obj(config)
+        config = config2obj(config)
 
     if sync:
         _sync(config, path)
@@ -294,22 +369,22 @@ def _sync(config, path: str) -> None:
     """
 
     def write(config):
-        with open(path, mode="wt", encoding="utf-8") as f:
-            if path.endswith(".yaml"):
+        with open(path, mode='wt', encoding='utf-8') as f:
+            if path.endswith('.yaml'):
                 if not yaml:
                     raise yaml_import_error
                 yaml.dump(config.dump(), f)
-            elif path.endswith(".json"):
+            elif path.endswith('.json'):
                 json.dump(config.dump(), f, ensure_ascii=False, indent=2)
             else:
-                raise TypeError(f"not support config file type {path}")
+                raise TypeError(f'not support config file type {path}')
 
     config._father(PropagateCallback(config, write))
     config._propagate()
 
 
-def sync(config, path: str = "config", raw_path: str = None):
-    path = raw_path or os.path.join("config", path)
+def sync(config, path: str = 'config', raw_path: str = None):
+    path = raw_path or os.path.join('config', path)
     _sync(config, path)
 
 
@@ -320,4 +395,4 @@ class Config:
     pass
 
 
-config: Config = read_config(raw_path="config", expect=Config)
+config: Config = read_config(raw_path='config', expect=Config)
